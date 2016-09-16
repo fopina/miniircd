6ed837fb347e6a2ffb966e190569bdb173d6bff3
Filipe Pina <fopina@gmail.com>
2016-09-16 13:16:48 +0100

    in pair

--- bots.py
+++ bots.py
@@ -1,16 +1,29 @@
 from irc.bot import SingleServerIRCBot
 import threading
+import time
+import string
+import random
 
 
 def join(c, e):
     c.join('#test')
 
 
+class A(SingleServerIRCBot):
+    def on_welcome(self, c, e):
+        c.join('#test')
+
+    def on_error(self, c, e):
+        print('ERROR:', repr(e))
+
+
 def newbot(nick):
-    c = SingleServerIRCBot([('localhost', 6667)], nick, nick)
-    c.connection.add_global_handler('endofmotd', join)
+    c = A([('127.0.0.1', 6667)], nick, nick)
     c.start()
 
-
-for i in xrange(100):
-    threading.Thread(target=newbot, args=('botbotbotbotbotbot%d' % i,)).start()
+for i in xrange(200):
+    threading.Thread(target=newbot, args=('%s%d' % (
+        ''.join(random.choice(string.letters) for _ in xrange(17)),
+        i
+    ),)).start()
+    time.sleep(0.1)
